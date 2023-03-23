f79a5ff985e9743448f96d01d15eabeb4c759d0a
devliet <101347493+devliet@users.noreply.github.com>
2023-03-23 12:05:46 +0100

    4 files modified, 1 added

--- src/app/hero-form/hero-form.component.ts
+++ src/app/hero-form/hero-form.component.ts
@@ -10,11 +10,15 @@ import { myHero } from '../myhero';
 export class HeroFormComponent {
   powers = ['Really Smart', 'Super Flexible', 'Super Hot', 'Weather Changer'];
 
-  model = new myHero(-1, '', '', '');
-
+  model = new myHero(18, 'Dr. IQ', this.powers[0], 'Chuck Overstreet');
+  //model = new myHero(18, '', '', '');
   submitted = false;
 
   onSubmit() {
     this.submitted = true;
+  }
+
+  newHero(){
+    this.model = new myHero(42, '', '');
   }
 }
